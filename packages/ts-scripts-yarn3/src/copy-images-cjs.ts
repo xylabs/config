648d2ebe53ff60107b03f27f383a319b5ac75026
Arie Trouw <arie.trouw@xylabs.com>
2022-08-05 20:56:17 -0700

    deploy

---
+++ packages/ts-scripts-yarn3/src/copy-images-cjs.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import { runSteps, ScriptStep, yarnWorkspaces } from './lib'
+
+const workspaces = yarnWorkspaces()
+
+const steps = workspaces.map<ScriptStep>(({ location }) => [
+  './node_modules/cpy-cli/cli.js',
+  [`${location}/src/**/*.jpg`, `${location}/src/**/*.png`, `${location}/src/**/*.gif`, `${location}/src/**/*.svg`, `${location}/dist/cjs`],
+])
+
+runSteps(
+  'Deps',
+  steps,
+  false,
+  workspaces.map(({ name }) => name)
+)
