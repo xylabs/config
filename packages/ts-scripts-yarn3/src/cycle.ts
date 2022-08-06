a87e5b2e842f8335524dea34534ddcba82881fff
Arie Trouw <arie.trouw@xylabs.com>
2022-08-05 20:04:47 -0700

    deploy

--- packages/ts-scripts-yarn3/src/cycle.ts
+++ packages/ts-scripts-yarn3/src/cycle.ts
@@ -2,9 +2,9 @@
 
 import { runSteps } from './lib'
 
-const rules = {
-  'import/no-cycle': [1, { maxDepth: 10 }],
-  'import/no-internal-modules': ['off'],
-}
+const rules = [
+  ['import/no-cycle', [1, { maxDepth: 10 }]],
+  ['import/no-internal-modules', ['off']],
+]
 
-runSteps('Cycle', [['./node_modules/eslint/bin/eslint.js', `${Object.entries(rules).map(([rule, value]) => ` --rule "${rule}: ${JSON.stringify(value)}"`)} .`]])
+runSteps('Cycle', [['./node_modules/eslint/bin/eslint.js', [...rules.map(([rule, value]) => ['--rule', `{${rule}: ${JSON.stringify(value)}}`]).flat(), '.']]])
