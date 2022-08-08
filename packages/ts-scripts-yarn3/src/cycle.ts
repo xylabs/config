800c16fe6a3267bd3855f2f7c645b1d79142ee47
Arie Trouw <arie.trouw@xylabs.com>
2022-08-08 14:12:45 -0700

    console.log removed

--- packages/ts-scripts-yarn3/src/cycle.ts
+++ packages/ts-scripts-yarn3/src/cycle.ts
@@ -4,6 +4,4 @@ import { runSteps } from './lib'
 
 const rules = ['"\'import/no-cycle\': [1, { maxDepth: 6 }]"', "\"'import/no-internal-modules': ['off']\""]
 
-console.log(rules.map((rule) => ['--rule', rule]).flat())
-
 runSteps('Cycle', [['node', ['./node_modules/eslint/bin/eslint.js', ...rules.map((rule) => ['--rule', rule]).flat(), '--cache', '.']]])
