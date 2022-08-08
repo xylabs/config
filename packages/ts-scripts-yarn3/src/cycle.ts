d893363003791ba6211679d8034aa22ef3f8c193
Arie Trouw <arie.trouw@xylabs.com>
2022-08-08 14:01:23 -0700

    cycle fixed

--- packages/ts-scripts-yarn3/src/cycle.ts
+++ packages/ts-scripts-yarn3/src/cycle.ts
@@ -2,11 +2,8 @@
 
 import { runSteps } from './lib'
 
-const rules = [
-  ['import/no-cycle', [1, { maxDepth: 6 }]],
-  ['import/no-internal-modules', ['off']],
-]
+const rules = ['"\'import/no-cycle\': [1, { maxDepth: 6 }]"', "\"'import/no-internal-modules': ['off']\""]
 
-runSteps('Cycle', [
-  ['node', ['./node_modules/eslint/bin/eslint.js', ...rules.map(([rule, value]) => ['--rule', `{${rule}: ${JSON.stringify(value)}}`]).flat(), '--cache', '.']],
-])
+console.log(rules.map((rule) => ['--rule', rule]).flat())
+
+runSteps('Cycle', [['node', ['./node_modules/eslint/bin/eslint.js', ...rules.map((rule) => ['--rule', rule]).flat(), '--cache', '.']]])
