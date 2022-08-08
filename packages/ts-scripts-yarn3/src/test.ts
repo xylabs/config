ba8ec7f1cf9c14417cd914aa3970a8c8ac7c43bb
Arie Trouw <arie.trouw@xylabs.com>
2022-08-08 09:53:28 -0700

    jest fixed

--- packages/ts-scripts-yarn3/src/test.ts
+++ packages/ts-scripts-yarn3/src/test.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import { runSteps } from './lib'
 
-runSteps('Test', [['yarn', './node_modules/jest/bin/jest.js .']])
+runSteps('Test', [['./node_modules/jest/bin/jest.js', '.']])
