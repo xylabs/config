6590f2692308cc43568f7a68436bd6fcc93b024a
Arie Trouw <arie.trouw@xylabs.com>
2022-08-09 06:41:21 -0700

    lint/cycle back in build/deploy

--- packages/ts-scripts-yarn3/src/deploy.ts
+++ packages/ts-scripts-yarn3/src/deploy.ts
@@ -5,8 +5,8 @@ import { runSteps } from './lib'
 runSteps('Deploy [Patch]', [
   ['yarn', 'workspaces foreach --all version patch --deferred'],
   ['yarn', 'build'],
+  ['yarn', 'cycle'],
   ['yarn', 'gen-docs'],
-  //['yarn', 'cycle'],
   ['yarn', 'version apply --all'],
   ['yarn', 'workspaces foreach -pt npm publish'],
 ])
