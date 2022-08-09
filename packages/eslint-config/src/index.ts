0e49074f594640e360978a7b31fb0abd7956628b
Arie Trouw <arie.trouw@xylabs.com>
2022-08-08 19:16:19 -0700

    eslint ignores

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['./typescript', './prettier', './import', './rules', './json', './markdown'],
-  ignorePatterns: ['**/dist', '**/build', '**/bin', '**/node_modules', '**/package.json', '**/.*'],
+  ignorePatterns: ['**/dist', '**/build', '**/bin', '**/node_modules', '**/package.json', '**/.*', '*.js*', '**/storybook-static'],
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
