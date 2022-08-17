fe60f0b4ec6eed38ec32f4dcfc04f54ed4af3fbb
Arie Trouw <arie.trouw@xylabs.com>
2022-08-17 08:39:33 -0700

    clean updated

--- packages/ts-scripts-yarn3/src/deploy-major.ts
+++ packages/ts-scripts-yarn3/src/deploy-major.ts
@@ -3,9 +3,11 @@
 import { runSteps } from './lib'
 
 runSteps('Deploy [Major]', [
+  ['yarn', 'clean'],
   ['yarn', 'workspaces foreach --all version minor --deferred'],
   ['yarn', 'cycle'],
   ['yarn', 'build'],
+  ['yarn', 'gen-docs'],
   ['yarn', 'version apply --all'],
   ['yarn', 'workspaces foreach -pt npm publish'],
 ])
