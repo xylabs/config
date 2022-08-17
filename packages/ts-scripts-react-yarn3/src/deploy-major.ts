fb4f4d90c5e88753607c29af03ef2f8fa2a68d0d
Arie Trouw <arie.trouw@xylabs.com>
2022-08-17 10:22:33 -0700

    moved cycle after build in deploy

--- packages/ts-scripts-react-yarn3/src/deploy-major.ts
+++ packages/ts-scripts-react-yarn3/src/deploy-major.ts
@@ -5,8 +5,8 @@ import { runSteps } from './lib'
 runSteps('Deploy [Major]', [
   ['yarn', 'clean'],
   ['yarn', 'workspaces foreach --all version minor --deferred'],
-  ['yarn', 'cycle'],
   ['yarn', 'build'],
+  ['yarn', 'cycle'],
   ['yarn', 'gen-docs'],
   ['yarn', 'version apply --all'],
   ['yarn', 'workspaces foreach -pt npm publish'],
