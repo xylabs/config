ab91b78ab46fa6b1d48763b46d7f845b5d94d087
Arie Trouw <arie.trouw@xylabs.com>
2022-08-18 18:30:34 -0700

    deploy prerelease

---
+++ packages/ts-scripts-yarn3/src/actions/deploy-prerelease.ts
@@ -0,0 +1,13 @@
+import { runSteps } from '../lib'
+
+export const deployPrerelease = () => {
+  runSteps('Deploy [RC]', [
+    ['yarn', 'clean'],
+    ['yarn', 'workspaces foreach --all version minor --deferred'],
+    ['yarn', 'build'],
+    ['yarn', 'cycle'],
+    ['yarn', 'gen-docs'],
+    ['yarn', 'version apply --all --prerelease'],
+    ['yarn', 'workspaces foreach -pt npm publish'],
+  ])
+}
---
+++ packages/ts-scripts-yarn3/src/deploy-prerelease.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { deployPrerelease } from './actions'
+
+deployPrerelease()
