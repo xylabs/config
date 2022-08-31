9f909e9473d940d35715ee402ce1cfd072e65a51
Arie Trouw <arie.trouw@xylabs.com>
2022-08-31 09:11:58 -0700

    deps ignore stories and specs

--- packages/ts-scripts-yarn3/src/actions/deps.ts
+++ packages/ts-scripts-yarn3/src/actions/deps.ts
@@ -3,7 +3,10 @@ import { runSteps, ScriptStep, yarnWorkspaces } from '../lib'
 export const deps = () => {
   const workspaces = yarnWorkspaces()
 
-  const steps = workspaces.map<ScriptStep>(({ location }) => ['node', ['./node_modules/depcheck/bin/depcheck.js', `${location}/.`]])
+  const steps = workspaces.map<ScriptStep>(({ location }) => [
+    'node',
+    ['./node_modules/depcheck/bin/depcheck.js', `${location}/.`, '--ignore-patterns=*.stories.*,*.spec.*'],
+  ])
 
   runSteps(
     'Deps',
