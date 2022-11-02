5bdcdda4ef69593428b2ccc1afe356a16c50679c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-02 13:48:24 -0500

    Increment major version

--- packages/ts-scripts-yarn3/src/actions/deploy-major.ts
+++ packages/ts-scripts-yarn3/src/actions/deploy-major.ts
@@ -3,7 +3,7 @@ import { runSteps } from '../lib'
 export const deployMajor = () => {
   return runSteps('Deploy [Major]', [
     ['yarn', 'clean'],
-    ['yarn', 'workspaces foreach --all version minor --deferred'],
+    ['yarn', 'workspaces foreach --all version major --deferred'],
     ['yarn', 'build'],
     ['yarn', 'cycle'],
     ['yarn', 'gen-docs'],
