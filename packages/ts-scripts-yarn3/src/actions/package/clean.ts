6cf97e11f886bf58562be3c4eafd9fc11bbce865
Arie Trouw <arie.trouw@xylabs.com>
2022-12-07 12:28:36 -0800

    Using TSC clean for cleaning and allowing incremental builds

--- packages/ts-scripts-yarn3/src/actions/package/clean.ts
+++ packages/ts-scripts-yarn3/src/actions/package/clean.ts
@@ -1,11 +1,14 @@
 import chalk from 'chalk'
-import { rmSync } from 'fs'
+
+import { runSteps } from '../../lib'
 
 export const packageClean = () => {
   const pkg = process.env.INIT_CWD
   const pkgName = process.env.npm_package_name
   console.log(chalk.green(`Cleaning [${pkgName}]`))
 
-  const dist = `${pkg}/dist`
-  rmSync(dist, { force: true, recursive: true })
+  return (
+    runSteps('Package Clean [ESM]', [['tsc', ['--build', '-p', `${pkg}/.tsconfig.build.esm.json`]]]) &&
+    runSteps('Package Clean [CJS]', [['tsc', ['--build', '-p', `${pkg}/.tsconfig.build.cjs.json`]]])
+  )
 }
