2250a17b3818ff4c336719f54f763c4a63a1be99
Arie Trouw <arie.trouw@xylabs.com>
2022-12-16 10:48:42 -0800

    upplug fixed

--- packages/ts-scripts-yarn3/src/actions/upplug.ts
+++ packages/ts-scripts-yarn3/src/actions/upplug.ts
@@ -2,9 +2,9 @@ import { runSteps } from '../lib'
 
 export const updateYarnPlugins = () => {
   return runSteps('Update Yarn Plugins [Outdated]', [
-    ['yarn', ['plugin', 'import', '@yarnpkg/plugin-outdated']],
-    ['yarn', ['plugin', 'import', '@yarnpkg/plugin-version']],
-    ['yarn', ['plugin', 'import', '@yarnpkg/plugin-interactive-tools']],
-    ['yarn', ['plugin', 'import', '@yarnpkg/plugin-workspace-tools']],
+    ['yarn', ['plugin', 'import', 'https://mskelton.dev/yarn-outdated/v3']],
+    ['yarn', ['plugin', 'import', 'version']],
+    ['yarn', ['plugin', 'import', 'interactive-tools']],
+    ['yarn', ['plugin', 'import', 'workspace-tools']],
   ])
 }
