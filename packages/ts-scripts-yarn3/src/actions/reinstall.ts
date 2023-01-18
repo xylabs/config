366b9add28456a0617533380b6cd22f0a2ae5adc
Arie Trouw <arie.trouw@xylabs.com>
2023-01-18 07:57:35 -0800

    reinstall update

--- packages/ts-scripts-yarn3/src/actions/reinstall.ts
+++ packages/ts-scripts-yarn3/src/actions/reinstall.ts
@@ -1,12 +1,26 @@
 import { closeSync, openSync, rmSync } from 'fs'
 
-import { runSteps } from '../lib'
+import { runSteps, yarnWorkspaces } from '../lib'
 
 export const reinstall = () => {
   console.log('Reinstall [Clear Lock File]')
   closeSync(openSync('./yarn.lock', 'w'))
+
   console.log('Reinstall [Clear Node Modules]')
-  const build = './node_modules'
-  rmSync(build, { force: true, recursive: true })
-  return runSteps('Reinstall', [['yarn', 'install --network-timeout 10000']])
+  const workspaces = yarnWorkspaces()
+  const result = workspaces
+    .map(({ location, name }) => {
+      const dist = `${location}/node_modules`
+      try {
+        rmSync(dist, { force: true, recursive: true })
+        return 0
+      } catch (ex) {
+        const error = ex as Error
+        console.error(`Reinstall [Clear Node Modules] Failed [${name}, ${error.message}]`)
+        return 1
+      }
+    })
+    .reduce((prev, result) => prev || result, 0)
+
+  return result || runSteps('Reinstall', [['yarn', 'install --network-timeout 10000']])
 }
