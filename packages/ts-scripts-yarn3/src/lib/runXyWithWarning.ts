47db40b349d23ebf0c2f2febe63fda4950ad7eb1
Arie Trouw <arie.trouw@xylabs.com>
2023-01-28 13:29:41 -0800

    forcing us of xy instead of bin script

--- packages/ts-scripts-yarn3/src/lib/runXyWithWarning.ts
+++ packages/ts-scripts-yarn3/src/lib/runXyWithWarning.ts
@@ -1,9 +1,7 @@
 import chalk from 'chalk'
 
-import { runXy } from './runXy'
-
 export const runXyWithWarning = (command: string) => {
-  console.warn(chalk.yellow(`WARNING: [${chalk.white(`yarn ${command}`)}] is being deprecated.`))
-  console.warn(chalk.gray(`use [${chalk.magenta(`yarn xy ${command}`)}] instead.`))
-  return runXy(command)
+  console.warn(chalk.yellow(`WARNING: [${chalk.white(`yarn ${command}`)}] is deprecated for XY Labs Scripts.`))
+  console.warn(chalk.gray(`Did you mean [${chalk.magenta(`yarn xy ${command}`)}]?`))
+  return 1
 }
