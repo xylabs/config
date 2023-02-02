c58b04d60c474e810c7452b0fa6a551b58fd41da
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-02-02 13:59:20 -0500

    convert logic to class

--- packages/ts-scripts-yarn3/src/actions/statics.ts
+++ packages/ts-scripts-yarn3/src/actions/statics.ts
@@ -12,60 +12,73 @@ type DependencyEntries = DependencyEntry[]
 
 interface Results {
   currentVersion: string | undefined
+  duplicateVersions: string[]
   errors: string[]
-  versions: string[]
 }
 
-const resultsFactory = (): Results => ({ currentVersion: undefined, errors: [], versions: [] })
+class DetectDuplicates {
+  private dependencyEntries: DependencyEntries
 
-const detectDuplicates = (output: DependencyEntries) => {
-  const result = output.reduce((acc, entry) => {
-    const entryVersion = Object.entries(entry.children).map(([k]) => k)[0]
+  constructor(output: string) {
+    this.dependencyEntries = this.formatOutput(output)
+  }
+
+  public detect() {
+    const result = this.dependencyEntries.reduce(this.detectReducer, this.resultsFactory())
+    if (result.errors.length) {
+      console.error('Errors: ', result.errors)
+      console.error('Duplicate Versions: ', result.duplicateVersions)
+      return 1
+    } else {
+      console.log('No Duplicates')
+      return 0
+    }
+  }
+
+  private detectReducer(acc: Results, entry: DependencyEntry) {
+    const version = Object.entries(entry.children).map(([k]) => k)[0]
 
     if (!acc.currentVersion) {
-      acc.currentVersion = entryVersion
+      acc.currentVersion = version
       return acc
     }
 
-    if (acc.currentVersion && acc.currentVersion !== entryVersion) {
-      const error = `currentVersion: ${acc.currentVersion} doesn't match entryVersion: ${entryVersion}`
+    if (acc.currentVersion && acc.currentVersion !== version) {
+      const error = `currentVersion: ${acc.currentVersion} doesn't match version: ${version}`
       acc.errors.push(error)
+      acc.duplicateVersions.push(version)
     }
     return acc
-  }, resultsFactory())
-
-  if (result.errors.length) {
-    console.error(result.errors)
-    return 1
-  } else {
-    console.log('No Duplicates')
-    return 0
   }
-}
 
-const formatOutput = (output: string) => {
-  const withCommas = output.replace(/\r\n/g, '').replace(/\n/g, ',')
-  const cleanCollection = withCommas.substring(0, withCommas.length - 1)
-  const collection = `[${cleanCollection}]`
-  return JSON.parse(collection)
+  private formatOutput(output: string) {
+    const withCommas = output.replace(/\r\n/g, '').replace(/\n/g, ',')
+    const cleanCollection = withCommas.substring(0, withCommas.length - 1)
+    const collection = `[${cleanCollection}]`
+    return JSON.parse(collection)
+  }
+
+  private resultsFactory = (): Results => ({ currentVersion: undefined, duplicateVersions: [], errors: [] })
 }
 
 export const confirmStaticPackages = () => {
   console.log(chalk.green('Confirming Static Packages'))
   return safeExit(() => {
+    let output: string
+
     try {
       const cmd = 'yarn why lodash --json'
-      const output = execSync(cmd).toString()
-      if (output) {
-        const parsedOutput = formatOutput(output)
-        return detectDuplicates(parsedOutput)
-      } else {
-        console.log('Library was not found')
-      }
-      return 0
+      output = execSync(cmd).toString()
     } catch (e) {
       console.error(`Error running yarn why: ${e}`)
       return 1
     }
+
+    if (output) {
+      return new DetectDuplicates(output).detect()
+    } else {
+      console.log('Library was not found')
+    }
+    return 0
   })
 }
