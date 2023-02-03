8c855ae553a4947a77d60f1ba25ddedece0a4a0e
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-02-03 16:27:46 -0500

    separate action for checking all dependencies

--- packages/ts-scripts-yarn3/src/actions/duplicate-deps.ts
+++ packages/ts-scripts-yarn3/src/actions/duplicate-deps.ts
@@ -2,16 +2,6 @@ import chalk from 'chalk'
 
 import { detect, parsedPackageJSON } from '../lib'
 
-const DefaultDependencies = ['axios', '@xylabs/pixel', 'react', 'graphql', 'react-router', '@mui/material', '@mui/styles', '@mui/system']
-
-export const confirmStaticDependencies = () => {
-  console.log(chalk.green('Confirming Static Dependencies'))
-
-  const statics = parsedPackageJSON()?.xy?.deps?.statics
-
-  return detect(statics, DefaultDependencies)
-}
-
 export const checkAllDependenciesForDuplicates = () => {
   console.log(chalk.green('Checking all Dependencies for Duplicates'))
 
