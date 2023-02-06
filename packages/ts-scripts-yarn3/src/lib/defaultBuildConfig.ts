3ad8e5e637c6edffb3605573324762e7574cc92a
Arie Trouw <arie.trouw@xylabs.com>
2023-02-06 15:11:19 -0800

    rootDirs update

--- packages/ts-scripts-yarn3/src/lib/defaultBuildConfig.ts
+++ packages/ts-scripts-yarn3/src/lib/defaultBuildConfig.ts
@@ -1,6 +1,7 @@
 export const defaultBuildConfig = {
   compilerOptions: {
     rootDir: 'src',
+    rootDirs: ['package.json'],
   },
   exclude: ['**/build', '**/dist', '**/node_modules', '**/*.spec.*', '**/*.spec', '**/*.stories.*', '**/*.example.*', '**/spec/*', '**/stories/*'],
   include: ['src'],
