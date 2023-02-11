05c00cd65f41d3fb76e828fc6f520486e668dcd2
Arie Trouw <arie.trouw@xylabs.com>
2023-02-10 21:50:04 -0800

    async - deploy

--- packages/ts-scripts-yarn3/src/actions/package/compile.ts
+++ packages/ts-scripts-yarn3/src/actions/package/compile.ts
@@ -2,5 +2,5 @@ import { packageCompileCjs } from './compile-cjs'
 import { packageCompileEsm } from './compile-esm'
 
 export const packageCompile = async () => {
-  return (await Promise.all([packageCompileEsm(), packageCompileCjs()])).reduce((prev, value) => prev + value)
+  return (await Promise.all([packageCompileEsm(), packageCompileCjs()])).reduce((prev, value) => prev + value, 0)
 }
--- packages/ts-scripts-yarn3/src/lib/runStepsAsync.ts
+++ packages/ts-scripts-yarn3/src/lib/runStepsAsync.ts
@@ -4,8 +4,8 @@ import { existsSync } from 'fs'
 
 import { ScriptStep } from './runSteps'
 
-export const runStepAsync = (step: ScriptStep, exitOnFail = true, message?: string) => {
-  return new Promise((resolve) => {
+export const runStepAsync = (step: ScriptStep, interactive = false, message?: string) => {
+  return new Promise<[number, string[]]>((resolve) => {
     const [command, args, config] = step
     if (message) {
       console.log(chalk.gray(message))
@@ -14,14 +14,15 @@ export const runStepAsync = (step: ScriptStep, exitOnFail = true, message?: stri
     if (command === 'node' && !existsSync(argList[0])) {
       throw Error(`File not found [${argList[0]}]`)
     }
+    const output: string[] = []
     spawn(command, Array.isArray(args) ? args : args.split(' '), {
       ...config,
       env: { FORCE_COLOR: '3', ...process.env },
       shell: true,
-      stdio: 'inherit',
-    }).on('close', (code) => {
-      if (code) {
-        if (exitOnFail) {
+      stdio: interactive ? 'inherit' : undefined,
+    })
+      .on('exit', (code) => {
+        if (code) {
           console.error(
             chalk.blue(
               `Command Exited With Non-Zero Result [${chalk.gray(code)}] | ${chalk.yellow(command)} ${chalk.white(
@@ -29,12 +30,16 @@ export const runStepAsync = (step: ScriptStep, exitOnFail = true, message?: stri
               )}`,
             ),
           )
+          resolve([code, output])
+        } else {
+          resolve([0, output])
         }
-        resolve(code)
-      } else {
-        resolve(0)
-      }
-    })
+      })
+      .stdout?.on('data', (data: Buffer) => {
+        if (!interactive) {
+          output.push(data.toString('utf8'))
+        }
+      })
   })
 }
 
@@ -42,10 +47,18 @@ export const runStepsAsync = async (name: string, steps: ScriptStep[], exitOnFai
   try {
     const pkgName = process.env.npm_package_name
     console.log(chalk.green(`${name} [${pkgName}]`))
-    for (let i = 0; i < steps.length; i++) {
-      await runStepAsync(steps[i], exitOnFail, messages?.[i])
+    const result = (
+      await Promise.all(
+        steps
+          .map<[ScriptStep, string | undefined]>((step, index) => [step, messages?.[index]])
+          .map(([step, message], index) => runStepAsync(step, index === 0, message)),
+      )
+    ).reduce<[number, string[]]>((prev, value) => [prev[0] + value[0], [...prev[1], ...value[1]]], [0, []])
+    console.log(result[1].join(''))
+    if (exitOnFail && result[0]) {
+      process.exit(result[0])
     }
-    return 0
+    return result[0]
   } catch (ex) {
     console.error(chalk.red(ex))
     process.exit(-1)
