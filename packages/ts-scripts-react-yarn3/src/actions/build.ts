e9b72653d7b51ec6027e28c177aea3728139a688
Arie Trouw <arie.trouw@xylabs.com>
2023-02-10 16:27:04 -0800

    fixing double compile for react

--- packages/ts-scripts-react-yarn3/src/actions/build.ts
+++ packages/ts-scripts-react-yarn3/src/actions/build.ts
@@ -2,7 +2,9 @@ import { runSteps } from '@xylabs/ts-scripts-yarn3'
 
 export const build = () => {
   return runSteps('Build', [
-    ['yarn', 'version patch --deferred'],
-    ['yarn', 'xy build-ci'],
+    ['yarn', ['xy', 'compile']],
+    ['yarn', 'xy lint'],
+    ['yarn', 'xy deps'],
+    ['ts-node-script', './scripts/sitemap.ts'],
   ])
 }
---
+++ packages/ts-scripts-react-yarn3/src/bin/compile.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { runXyWithWarning } from "@xylabs/ts-scripts-yarn3"
+
+runXyWithWarning('compile')
