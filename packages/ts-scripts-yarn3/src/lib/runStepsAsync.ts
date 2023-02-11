3615cb882c180216521ef3e0f7c378a27ee0f4e1
Arie Trouw <arie.trouw@xylabs.com>
2023-02-10 21:55:17 -0800

    revert async

--- packages/ts-scripts-yarn3/src/lib/runStepsAsync.ts
+++ packages/ts-scripts-yarn3/src/lib/runStepsAsync.ts
@@ -4,8 +4,8 @@ import { existsSync } from 'fs'
 
 import { ScriptStep } from './runSteps'
 
-export const runStepAsync = (step: ScriptStep, interactive = false, message?: string) => {
-  return new Promise<[number, string[]]>((resolve) => {
+export const runStepAsync = (step: ScriptStep, exitOnFail = true, message?: string) => {
+  return new Promise((resolve) => {
     const [command, args, config] = step
     if (message) {
       console.log(chalk.gray(message))
@@ -14,15 +14,14 @@ export const runStepAsync = (step: ScriptStep, interactive = false, message?: st
     if (command === 'node' && !existsSync(argList[0])) {
       throw Error(`File not found [${argList[0]}]`)
     }
-    const output: string[] = []
     spawn(command, Array.isArray(args) ? args : args.split(' '), {
       ...config,
       env: { FORCE_COLOR: '3', ...process.env },
       shell: true,
-      stdio: interactive ? 'inherit' : undefined,
-    })
-      .on('exit', (code) => {
-        if (code) {
+      stdio: 'inherit',
+    }).on('close', (code) => {
+      if (code) {
+        if (exitOnFail) {
           console.error(
             chalk.blue(
               `Command Exited With Non-Zero Result [${chalk.gray(code)}] | ${chalk.yellow(command)} ${chalk.white(
@@ -30,16 +29,12 @@ export const runStepAsync = (step: ScriptStep, interactive = false, message?: st
               )}`,
             ),
           )
-          resolve([code, output])
-        } else {
-          resolve([0, output])
         }
-      })
-      .stdout?.on('data', (data: Buffer) => {
-        if (!interactive) {
-          output.push(data.toString('utf8'))
-        }
-      })
+        resolve(code)
+      } else {
+        resolve(0)
+      }
+    })
   })
 }
 
@@ -47,18 +42,10 @@ export const runStepsAsync = async (name: string, steps: ScriptStep[], exitOnFai
   try {
     const pkgName = process.env.npm_package_name
     console.log(chalk.green(`${name} [${pkgName}]`))
-    const result = (
-      await Promise.all(
-        steps
-          .map<[ScriptStep, string | undefined]>((step, index) => [step, messages?.[index]])
-          .map(([step, message], index) => runStepAsync(step, index === 0, message)),
-      )
-    ).reduce<[number, string[]]>((prev, value) => [prev[0] + value[0], [...prev[1], ...value[1]]], [0, []])
-    console.log(result[1].join(''))
-    if (exitOnFail && result[0]) {
-      process.exit(result[0])
+    for (let i = 0; i < steps.length; i++) {
+      await runStepAsync(steps[i], exitOnFail, messages?.[i])
     }
-    return result[0]
+    return 0
   } catch (ex) {
     console.error(chalk.red(ex))
     process.exit(-1)
