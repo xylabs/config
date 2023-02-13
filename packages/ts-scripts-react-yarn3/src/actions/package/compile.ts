f67b918dfc9e72423719ec9029919e64b9e88209
Arie Trouw <arie.trouw@xylabs.com>
2023-02-13 08:26:53 -0800

    sitemap in build

--- packages/ts-scripts-react-yarn3/src/actions/package/compile.ts
+++ packages/ts-scripts-react-yarn3/src/actions/package/compile.ts
@@ -1,9 +1,15 @@
 import { runSteps, runStepsAsync } from '@xylabs/ts-scripts-yarn3'
 
 export const packageCompile = () => {
-  return runSteps('Package Compile', [['yarn', ['react-scripts', 'build']]])
+  return runSteps('Package Compile', [
+    ['yarn', ['react-scripts', 'build']],
+    ['ts-node-script', './scripts/sitemap.ts'],
+  ])
 }
 
 export const packageCompileAsync = () => {
-  return runStepsAsync('Package Compile', [['yarn', ['react-scripts', 'build']]])
+  return runStepsAsync('Package Compile', [
+    ['yarn', ['react-scripts', 'build']],
+    ['ts-node-script', './scripts/sitemap.ts'],
+  ])
 }
