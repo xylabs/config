7e5083abb1eb0cd13bfe7951f63f5bc6d3254858
Arie Trouw <arie.trouw@xylabs.com>
2023-02-13 08:40:26 -0800

    build calling package-compile

--- packages/ts-scripts-yarn3/src/actions/compile.ts
+++ packages/ts-scripts-yarn3/src/actions/compile.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk'
 
-import { runStepsAsync, ScriptStep } from '../lib'
+import { runStepsAsync } from '../lib'
 
 export interface CompileParams {
   incremental?: boolean
@@ -22,32 +22,24 @@ export const compile = async ({ verbose, target, pkg, incremental }: CompilePara
 
 export const compilePackage = ({ verbose, target, pkg }: CompilePackageParams) => {
   const verboseOptions = verbose ? ['-v'] : []
-  const cjsSteps: ScriptStep[] = target === 'cjs' || !target ? [['yarn', ['workspace', pkg, 'run', 'package-compile-cjs', ...verboseOptions]]] : []
+  const targetOptions = target ? ['-t', target] : []
 
-  const esmSteps: ScriptStep[] = target === 'esm' || !target ? [['yarn', ['workspace', pkg, 'run', 'package-compile-esm', ...verboseOptions]]] : []
-
-  return runStepsAsync(`Compile [${pkg}]`, [...esmSteps, ...cjsSteps])
+  return runStepsAsync(`Compile [${pkg}]`, [['yarn', ['workspace', pkg, 'run', 'package-compile-cjs', ...verboseOptions, ...targetOptions]]])
 }
 
 export const compileAll = async ({ jobs, verbose, target, incremental }: CompileParams) => {
   const start = Date.now()
   const verboseOptions = verbose ? ['-v'] : []
+  const targetOptions = target ? ['-t', target] : []
   const incrementalOptions = incremental ? ['--since', '-ptA'] : ['-ptA']
   const jobsOptions = jobs ? ['-j', `${jobs}`] : []
   if (jobs) {
     console.log(chalk.blue(`Jobs set to [${jobs}]`))
   }
-  const cjsSteps: ScriptStep[] =
-    target === 'cjs' || !target
-      ? [['yarn', ['workspaces', 'foreach', ...incrementalOptions, ...jobsOptions, 'run', 'package-compile-cjs', ...verboseOptions]]]
-      : []
 
-  const esmSteps: ScriptStep[] =
-    target === 'esm' || !target
-      ? [['yarn', ['workspaces', 'foreach', ...incrementalOptions, ...jobsOptions, 'run', 'package-compile-esm', ...verboseOptions]]]
-      : []
-
-  const result = await runStepsAsync(`Compile [All${incremental ? '-Incremental' : ''}]`, [...esmSteps, ...cjsSteps])
+  const result = await runStepsAsync(`Compile [All${incremental ? '-Incremental' : ''}]`, [
+    ['yarn', ['workspaces', 'foreach', ...incrementalOptions, ...jobsOptions, 'run', 'package-compile', ...verboseOptions, ...targetOptions]],
+  ])
   console.log(`${chalk.gray('Compiled in')} [${chalk.magenta(((Date.now() - start) / 1000).toFixed(2))}] ${chalk.gray('seconds')}`)
   return result
 }
