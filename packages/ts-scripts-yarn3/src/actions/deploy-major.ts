79dcb90eba0f54485e9806d9fcefc6350e41e41b
Arie Trouw <arie.trouw@xylabs.com>
2023-02-13 14:39:34 -0800

    exit on fail and rebuild for deploy

--- packages/ts-scripts-yarn3/src/actions/deploy-major.ts
+++ packages/ts-scripts-yarn3/src/actions/deploy-major.ts
@@ -2,9 +2,8 @@ import { runSteps } from '../lib'
 
 export const deployMajor = () => {
   return runSteps('Deploy [Major]', [
-    ['yarn', 'xy clean'],
     ['yarn', 'workspaces foreach --all version major --deferred'],
-    ['yarn', 'xy build'],
+    ['yarn', 'xy rebuild'],
     ['yarn', 'xy cycle'],
     ['yarn', 'xy gen-docs'],
     ['yarn', 'version apply --all'],
--- packages/ts-scripts-yarn3/src/actions/deploy.ts
+++ packages/ts-scripts-yarn3/src/actions/deploy.ts
@@ -2,9 +2,8 @@ import { runSteps } from '../lib'
 
 export const deploy = () => {
   return runSteps('Deploy [Patch]', [
-    ['yarn', 'xy clean'],
     ['yarn', 'workspaces foreach --all version patch --deferred'],
-    ['yarn', 'xy build'],
+    ['yarn', 'xy rebuild'],
     ['yarn', 'xy cycle'],
     ['yarn', 'xy gen-docs'],
     ['yarn', 'version apply --all'],
