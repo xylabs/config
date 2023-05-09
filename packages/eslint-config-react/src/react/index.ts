d5aad79176feb3eb920bc136fdffe7b67c31a9b5
Arie Trouw <arie.trouw@xylabs.com>
2023-05-08 18:09:04 -0700

    react lint only for tsx files

--- packages/eslint-config-react/src/react/index.ts
+++ packages/eslint-config-react/src/react/index.ts
@@ -1,20 +1,25 @@
 module.exports = {
-  extends: ['@xylabs', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    'import/no-nodejs-modules': ['error'],
-    'react-hooks/exhaustive-deps': [
-      'warn',
-      {
-        additionalHooks: '(useAsyncEffect)',
+  overrides: [
+    {
+      extends: ['@xylabs', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+      files: ['*.tsx'],
+      plugins: ['react', 'react-hooks'],
+      rules: {
+        'import/no-nodejs-modules': ['error'],
+        'react-hooks/exhaustive-deps': [
+          'warn',
+          {
+            additionalHooks: '(useAsyncEffect)',
+          },
+        ],
+        'react/prop-types': ['off'],
+        'react/react-in-jsx-scope': ['off'],
       },
-    ],
-    'react/prop-types': ['off'],
-    'react/react-in-jsx-scope': ['off'],
-  },
-  settings: {
-    react: {
-      version: 'detect',
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
     },
-  },
+  ],
 }
