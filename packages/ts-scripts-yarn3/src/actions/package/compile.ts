96bbe819ee17cb22f61799b543a76d3b82a7ff2f
Arie Trouw <arie.trouw@xylabs.com>
2023-06-19 15:19:08 -0700

    Not building CJS for modules

--- packages/ts-scripts-yarn3/src/actions/package/compile.ts
+++ packages/ts-scripts-yarn3/src/actions/package/compile.ts
@@ -1,6 +1,17 @@
+import { readFileSync } from 'fs'
+
 import { packageCompileCjs } from './compile-cjs'
 import { packageCompileEsm } from './compile-esm'
 
 export const packageCompile = async () => {
-  return (await Promise.all([packageCompileEsm(), packageCompileCjs()])).reduce((prev, value) => prev + value, 0)
+  const pkg = process.env.INIT_CWD
+
+  const pkgConfig = readFileSync(`${pkg}/package.json`, { encoding: 'utf8' })
+
+  const config = JSON.parse(pkgConfig)
+
+  return (await Promise.all(config.type === 'module' ? [packageCompileEsm()] : [packageCompileEsm(), packageCompileCjs()])).reduce(
+    (prev, value) => prev + value,
+    0,
+  )
 }
