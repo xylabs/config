a95544cd5dd3c50f60e99aa203503c3f73e8d94a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-29 14:45:12 -0500

    Remove docs only 1 level deep

--- packages/ts-scripts-yarn3/src/actions/clean-docs.ts
+++ packages/ts-scripts-yarn3/src/actions/clean-docs.ts
@@ -6,8 +6,6 @@ export const cleanDocs = () => {
   const pkg = process.env.INIT_CWD ?? '.'
   const pkgName = process.env.npm_package_name
   console.log(chalk.green(`Cleaning Docs [${pkgName}]`))
-
-  rimrafSync(path.join(pkg, '**/docs'), { glob: true })
-
+  rimrafSync(path.join(pkg, 'docs'), { glob: true })
   return 0
 }
