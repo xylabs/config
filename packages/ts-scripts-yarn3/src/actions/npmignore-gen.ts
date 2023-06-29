a1f3ad8d91e06110964a7e31237b0620501bb0c8
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-29 15:59:24 -0500

    Towards a generic ignore file generator

--- packages/ts-scripts-yarn3/src/actions/npmignore-gen.ts
+++ packages/ts-scripts-yarn3/src/actions/npmignore-gen.ts
@@ -2,12 +2,14 @@ import chalk from 'chalk'
 
 import { INIT_CWD, readNonEmptyLines, union, writeLines, yarnWorkspace, yarnWorkspaces } from '../lib'
 
-const readEntries = (location: string): string[] => readNonEmptyLines(`${location}/.npmignore`)
-const writeEntries = (location: string, entries: string[]) => writeLines(`${location}/.npmignore`, entries)
+const filename = '.npmignore'
+
+const readEntries = (location: string): string[] => readNonEmptyLines(`${location}/${filename}`)
+const writeEntries = (location: string, entries: string[]) => writeLines(`${location}/${filename}`, entries)
 const mergeEntries = (a: string[], b: string[]): string[] => [...union(a, b)].sort()
 
-export const npmignoreGen = (pkg?: string) => {
-  console.log(chalk.green('Generate .npmignore Files'))
+export const generateIgnoreFiles = (pkg?: string) => {
+  console.log(chalk.green(`Generate ${filename} Files`))
   const cwd = INIT_CWD() ?? '.'
   const workspaces = pkg ? [yarnWorkspace(pkg)] : yarnWorkspaces()
   const results = workspaces.map(({ location, name }) => {
@@ -16,7 +18,7 @@ export const npmignoreGen = (pkg?: string) => {
       return 0
     } catch (ex) {
       const error = ex as Error
-      console.error(`Generate .npmignore Files [${name}] [${error.message}]`)
+      console.error(`Generate ${filename} Files [${name}] [${error.message}]`)
       return 1
     }
   })
