cd4fe30c38117b13abadb93f6c6df32c69ba8d71
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-29 14:53:49 -0500

    Clean docs per workspace

--- packages/ts-scripts-yarn3/src/actions/clean-docs.ts
+++ packages/ts-scripts-yarn3/src/actions/clean-docs.ts
@@ -2,10 +2,11 @@ import chalk from 'chalk'
 import path from 'path'
 import { rimrafSync } from 'rimraf'
 
+import { yarnWorkspaces } from '../lib'
+
 export const cleanDocs = () => {
-  const pkg = process.env.INIT_CWD ?? '.'
   const pkgName = process.env.npm_package_name
   console.log(chalk.green(`Cleaning Docs [${pkgName}]`))
-  rimrafSync(path.join(pkg, 'docs'), { glob: true })
+  yarnWorkspaces().map(({ location }) => rimrafSync(path.join(location, 'docs'), { glob: true }))
   return 0
 }
