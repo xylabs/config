779c0381b514f6357f92c0a14033d59820f23159
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-29 14:32:11 -0500

    Add tests for yarn workspace

---
+++ packages/ts-scripts-yarn3/src/lib/yarn/workspace/spec/yarnWorkspace.spec.ts
@@ -0,0 +1,18 @@
+import { yarnWorkspace } from '../yarnWorkspace'
+
+describe('yarnWorkspace', () => {
+  describe('when workspace', () => {
+    describe('exists', () => {
+      it('returns the workspace', () => {
+        const workspace = yarnWorkspace('@xylabs/ts-scripts-yarn3')
+        expect(workspace.name).toBeString()
+        expect(workspace.location).toBeString()
+      })
+    })
+    describe('does not exist', () => {
+      it('throws', () => {
+        expect(() => yarnWorkspace('@xylabs/foo-bar')).toThrow()
+      })
+    })
+  })
+})
