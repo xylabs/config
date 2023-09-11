85837d0108ed7a2dccf289aff56896f97da9907c
Arie Trouw <arie.trouw@xylabs.com>
2023-09-11 12:22:10 -0700

    switch -A to -R for compiles to prevent races?

--- packages/ts-scripts-yarn3/src/actions/recompile.ts
+++ packages/ts-scripts-yarn3/src/actions/recompile.ts
@@ -31,7 +31,7 @@ export const recompileAll = async ({ jobs, verbose, target, incremental }: Recom
   const start = Date.now()
   const verboseOptions = verbose ? ['-v'] : []
   const targetOptions = target ? ['-t', target] : []
-  const incrementalOptions = incremental ? ['--since', '-ptA'] : ['-ptA']
+  const incrementalOptions = incremental ? ['--since', '-ptR'] : ['-ptR']
   const jobsOptions = jobs ? ['-j', `${jobs}`] : []
   if (jobs) {
     console.log(chalk.blue(`Jobs set to [${jobs}]`))
