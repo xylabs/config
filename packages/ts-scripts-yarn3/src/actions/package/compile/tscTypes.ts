797a3ae911a30040501bb661272380a84dc9a0e9
Arie Trouw <arie.trouw@xylabs.com>
2023-09-25 13:46:23 -0700

    deploy 3.0.56

--- packages/ts-scripts-yarn3/src/actions/package/compile/tscTypes.ts
+++ packages/ts-scripts-yarn3/src/actions/package/compile/tscTypes.ts
@@ -5,6 +5,7 @@ import { CompilerOptions, DiagnosticCategory } from 'typescript'
 import { CompileParams } from './CompileParams'
 import { copyTypeFiles } from './copyTypeFiles'
 import { getCompilerOptions } from './getCompilerOptions'
+import chalk from 'chalk'
 
 export const packageCompileTscTypes = async (params?: CompileParams, compilerOptionsParam?: CompilerOptions, generateMts = true): Promise<number> => {
   const pkg = process.env.INIT_CWD
@@ -32,6 +33,22 @@ export const packageCompileTscTypes = async (params?: CompileParams, compilerOpt
   }).emit()
 
   const diagResults = result.diagnostics.map((value) => value.category === DiagnosticCategory.Error).length
+  result.diagnostics.forEach((diag) => {
+    switch (diag.category) {
+      case DiagnosticCategory.Error:
+        console.error(chalk.red(diag.messageText))
+        console.error(chalk.grey(JSON.stringify(diag)))
+        break
+      case DiagnosticCategory.Warning:
+        console.error(chalk.yellow(diag.messageText))
+        console.error(chalk.grey(JSON.stringify(diag)))
+        break
+      case DiagnosticCategory.Suggestion:
+        console.error(chalk.white(diag.messageText))
+        console.error(chalk.grey(JSON.stringify(diag)))
+        break
+    }
+  })
   if (generateMts) {
     await copyTypeFiles(compilerOptions)
   }
