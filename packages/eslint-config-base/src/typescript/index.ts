f36c7db21c05709dbf511313a64700d7f0471f78
Arie Trouw <arie.trouw@xylabs.com>
2023-11-08 09:57:30 -0800

    deploy 3.1.19

--- packages/eslint-config-base/src/typescript/index.ts
+++ packages/eslint-config-base/src/typescript/index.ts
@@ -5,7 +5,6 @@ export const typescriptConfig: ESLint.ConfigData = {
   overrides: [
     {
       files: ['*.ts*', '*.d.ts*'],
-      parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint'],
       rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
