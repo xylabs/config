1d2b09dc2f374d8a64f11ad2ecc38b6c1087bca9
Arie Trouw <arie.trouw@xylabs.com>
2024-03-08 11:20:03 -0800

    one more clean speed improvement - 3.5.2

--- packages/ts-scripts-yarn3/src/actions/package/clean-outputs.ts
+++ packages/ts-scripts-yarn3/src/actions/package/clean-outputs.ts
@@ -1,7 +1,7 @@
-import { rm } from 'node:fs/promises'
 import path from 'node:path'
 
 import chalk from 'chalk'
+import { rimraf } from 'rimraf'
 
 export const packageCleanOutputs = async () => {
   const pkg = process.env.INIT_CWD ?? '.'
@@ -9,7 +9,7 @@ export const packageCleanOutputs = async () => {
   const folders: string[] = [path.join(pkg, 'dist'), path.join(pkg, 'build'), path.join(pkg, 'docs')]
   console.log(chalk.green(`Cleaning Outputs [${pkgName}]`))
 
-  await Promise.all(folders.map((folder) => rm(folder, { force: true, recursive: true })))
+  await Promise.all(folders.map((folder) => rimraf(folder)))
 
   return 0
 }
--- packages/ts-scripts-yarn3/src/actions/package/clean-typescript.ts
+++ packages/ts-scripts-yarn3/src/actions/package/clean-typescript.ts
@@ -1,7 +1,7 @@
-import { rm } from 'node:fs/promises'
 import path from 'node:path'
 
 import chalk from 'chalk'
+import { rimraf } from 'rimraf'
 
 export const packageCleanTypescript = async () => {
   const pkg = process.env.INIT_CWD ?? '.'
@@ -9,7 +9,7 @@ export const packageCleanTypescript = async () => {
   console.log(chalk.green(`Cleaning Typescript [${pkgName}]`))
   const files: string[] = [path.join(pkg, '*.tsbuildinfo'), path.join(pkg, '.tsconfig.*'), path.join(pkg, '.eslintcache')]
 
-  await Promise.all(files.map((file) => rm(file, { force: true, recursive: true })))
+  await Promise.all(files.map((file) => rimraf(file)))
 
   return 0
 }
