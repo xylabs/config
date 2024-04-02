c504f70c3c34eab08029e8042b1ddebcbca642ac
Arie Trouw <arie.trouw@xylabs.com>
2024-04-02 09:54:33 -0700

    updo and parallel deploy

--- packages/ts-scripts-yarn3/src/actions/deploy.ts
+++ packages/ts-scripts-yarn3/src/actions/deploy.ts
@@ -2,11 +2,11 @@ import { runSteps } from '../lib'
 
 export const deploy = () => {
   return runSteps('Deploy [Patch]', [
-    ['yarn', 'workspaces foreach --all version patch --deferred'],
+    ['yarn', 'workspaces foreach --all --parallel version patch --deferred'],
     ['yarn', 'xy clean'],
     ['yarn', 'xy build'],
     ['yarn', 'xy cycle'],
     ['yarn', 'version apply --all'],
-    ['yarn', 'workspaces foreach --all npm publish'],
+    ['yarn', 'workspaces foreach --all --parallel npm publish'],
   ])
 }
