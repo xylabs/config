896d9ece034b19a71839350b9dede8adbbd1ec91
Arie Trouw <arie.trouw@xylabs.com>
2024-07-27 13:34:45 -0700

    disabled sonarjs/no-small-switch

--- packages/eslint-config-flat/src/sonar/index.ts
+++ packages/eslint-config-flat/src/sonar/index.ts
@@ -4,4 +4,8 @@ import sonarjsPlugin from 'eslint-plugin-sonarjs'
 export const sonarConfig: Linter.FlatConfig = {
   ignores: ['.yarn/**', 'jest.config.cjs', '**/dist/**', 'dist', 'build/**', 'node_modules/**'],
   ...sonarjsPlugin.configs.recommended,
+  rules: {
+    ...sonarjsPlugin.configs.recommended.rules,
+    'sonarjs/no-small-switch': ['off'],
+  },
 }
