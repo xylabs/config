2b91147c28ab5a5e202357c2322cb57014217dd1
Arie Trouw <arie.trouw@xylabs.com>
2024-08-02 13:48:17 -0700

    deploy 3.15.4

--- packages/ts-scripts-yarn3/src/actions/package/compile/getCompilerOptions.ts
+++ packages/ts-scripts-yarn3/src/actions/package/compile/getCompilerOptions.ts
@@ -1,26 +1,27 @@
 import { createRequire } from 'node:module'
 
 import { merge } from 'lodash-es'
+import { TsConfig } from 'tsc-prog'
 import { CompilerOptions, findConfigFile, readConfigFile, sys } from 'typescript'
 
-export const getCompilerOptionsJSONFollowExtends = (filename: string): CompilerOptions => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let opts: any = {}
-  const config = readConfigFile(filename, sys.readFile).config
+const getNested = (config: TsConfig): CompilerOptions => {
   if (config.extends) {
     const require = createRequire(import.meta.url)
-    opts = require(config.extends)
-  }
-  if (config?.error) {
-    throw new Error(`getCompilerOptionsJSONFollowExtends failed ${JSON.stringify(config?.error?.messageText, null, 2)}`)
+    const opts = require(config.extends)
+    return { ...getNested(opts), ...config.compilerOptions } as CompilerOptions
   }
 
-  return { ...opts.compilerOptions, ...config.compilerOptions }
+  return config.compilerOptions as CompilerOptions
 }
 
-export const getCompilerOptions = async (options?: CompilerOptions, tsconfig: string = 'tsconfig.json'): Promise<CompilerOptions> => {
+export const getCompilerOptionsJSONFollowExtends = (filename: string): CompilerOptions => {
+  const config = readConfigFile(filename, sys.readFile).config
+  return getNested(config)
+}
+
+export const getCompilerOptions = (options?: CompilerOptions, tsconfig: string = 'tsconfig.json'): CompilerOptions => {
   const configFileName = findConfigFile('./', sys.fileExists, tsconfig)
-  const configFileCompilerOptions = configFileName ? await getCompilerOptionsJSONFollowExtends(configFileName) : undefined
+  const configFileCompilerOptions = configFileName ? getCompilerOptionsJSONFollowExtends(configFileName) : undefined
 
   return merge({}, configFileCompilerOptions, options)
 }
