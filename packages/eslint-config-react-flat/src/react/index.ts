0606813df80f95edd81444c1e8ffec2a68a383e9
Arie Trouw <arie.trouw@xylabs.com>
2024-08-03 13:13:26 -0700

    deploy 3.15.11

--- packages/eslint-config-react-flat/src/react/index.ts
+++ packages/eslint-config-react-flat/src/react/index.ts
@@ -1,13 +1,13 @@
 import tsParser from '@typescript-eslint/parser'
 import { Linter } from 'eslint'
 import reactPlugin from 'eslint-plugin-react'
-// import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
 import globals from 'globals'
 
 export const reactConfig: Linter.Config = {
   plugins: {
-    react: reactPlugin,
-    // 'react-hooks': reactHooksPlugin,
+    'react': reactPlugin,
+    'react-hooks': reactHooksPlugin,
   },
   files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
   languageOptions: {
@@ -25,13 +25,13 @@ export const reactConfig: Linter.Config = {
   },
   rules: {
     ...reactPlugin.configs.flat.recommended.rules,
-    /* 'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
       'warn',
       {
         additionalHooks: '(useAsyncEffect|usePromise)',
       },
-    ], */
+    ],
     'react/prop-types': ['off'],
     'react/react-in-jsx-scope': ['warn'],
   },
