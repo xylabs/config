97d79fc852eff9ff2b99268c34c6b0bec0df58c1
Arie Trouw <arie.trouw@xylabs.com>
2024-08-05 15:16:01 -0700

    deploy 3.15.15

--- packages/eslint-config-react-flat/src/react/index.ts
+++ packages/eslint-config-react-flat/src/react/index.ts
@@ -1,11 +1,41 @@
-import reactPlugin from '@eslint-react/eslint-plugin'
+import eslintReactPlugin from '@eslint-react/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import { Linter } from 'eslint'
+import reactPlugin from 'eslint-plugin-react'
+import globals from 'globals'
 
 export const reactConfig = {
   files: ['**/*.{ts,tsx}'],
-  ...reactPlugin.configs.recommended,
+  ...eslintReactPlugin.configs.recommended,
+  plugins: {
+    react: reactPlugin,
+    ...eslintReactPlugin.configs.recommended.plugins,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   languageOptions: {
     parser: tsParser,
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    globals: {
+      ...globals.browser,
+    },
+  },
+  rules: {
+    ...eslintReactPlugin.configs.recommended.rules,
+    ...reactPlugin.configs.flat.recommended.rules,
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: '(useAsyncEffect|usePromise)',
+      },
+    ],
+    'react/prop-types': ['off'],
   },
 } as unknown as Linter.Config
---
+++ packages/eslint-config-react-flat/src/types.d.ts
@@ -0,0 +1,3 @@
+declare module 'eslint-plugin-react' {
+  const configs: Linter.Plugin
+}
