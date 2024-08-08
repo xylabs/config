587abda2a8c35403506d2a8c8630ffc6934f2ff3
Arie Trouw <arie.trouw@xylabs.com>
2024-08-07 18:19:46 -0700

    deploy 4.0.0-rc.12

--- packages/ts-scripts-yarn3/src/actions/cycle.ts
+++ packages/ts-scripts-yarn3/src/actions/cycle.ts
@@ -1,23 +1,18 @@
 import { cwd } from 'node:process'
 
 import { ESLint } from 'eslint'
-import importPlugin from 'eslint-plugin-import'
 
 export const cycle = async () => {
   const eslint = new ESLint({ fix: false, overrideConfig: {
-    plugins: { import: importPlugin },
     rules: {
       'import/no-cycle': ['error', { maxDepth: 10 }],
     },
   } })
-  const configFile = await eslint.findConfigFile()
-  console.log('Config file:', configFile)
   const results = await eslint.lintFiles(['src/**/*.ts*', 'packages/**/src/**/*.ts*'])
 
   const formatter = await eslint.loadFormatter('stylish')
   const resultText = formatter.format(results, { cwd: cwd(), rulesMeta: {} })
   console.log(resultText)
 
-  console.log('Lint Errors:', results.length)
   return results.length
 }
