1cd1edbb5797bd29eceb681aa2d753b1fa0eab2c
Arie Trouw <arie.trouw@xylabs.com>
2025-02-03 09:42:14 -0800

    Build Update
    Doing both tsc and tsup when building

--- packages/ts-scripts-yarn3/src/actions/build.ts
+++ packages/ts-scripts-yarn3/src/actions/build.ts
@@ -25,6 +25,7 @@ export const build = async ({
 
   const result = await runStepsAsync(`Build${incremental ? '-Incremental' : ''} [${pkg ?? 'All'}]`, [
     ['yarn', ['xy', 'compile', ...pkgOptions, ...targetOptions, ...verboseOptions, ...jobsOptions, ...incrementalOptions, '--types', 'tsc']],
+    ['yarn', ['xy', 'compile', ...pkgOptions, ...targetOptions, ...verboseOptions, ...jobsOptions, ...incrementalOptions, '--types', 'tsup']],
     ['yarn', ['xy', 'lint', ...pkgOptions, ...verboseOptions, ...incrementalOptions]],
     ['yarn', ['xy', 'deps', ...pkgOptions, ...verboseOptions, ...jobsOptions, ...incrementalOptions]],
   ])
