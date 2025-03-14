819a0e8f49ff952cce824ee2f1de91c7582f45fe
Arie Trouw <arie.trouw@xylabs.com>
2025-03-13 17:55:31 -0700

    Deploy
    6.0.9

--- packages/ts-scripts-react-yarn3/xy.config.ts
+++ packages/ts-scripts-react-yarn3/xy.config.ts
@@ -1,9 +1,9 @@
 import type { XyTsupConfig } from '@xylabs/ts-scripts-yarn3'
 const config: XyTsupConfig = {
   compile: {
-    browser: { src: {} },
+    browser: {},
     entryMode: 'all',
-    node: { src: {} },
+    node: {},
   },
 }
 
