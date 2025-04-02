cec6d1ed33ef45db896acde98c54f72281356d54
Arie Trouw <arie.trouw@xylabs.com>
2025-04-02 13:52:21 -0700

    Adds linting and fixes build process.
    Adds linting and Knip to the build process to improve code quality and prevent errors.
    Fixes the clean script by adding the --glob flag to ensure proper removal of directories.
    Removes unnecessary chalk import and sonar config.

--- packages/ts-scripts-yarn3/src/actions/package/compile/compileTypes.ts
+++ packages/ts-scripts-yarn3/src/actions/package/compile/compileTypes.ts
@@ -1,11 +1,8 @@
-import chalk from 'chalk'
-
 import { loadConfig } from '../../../lib/index.ts'
 import { packageCompileTscTypes } from './packageCompileTscTypes.ts'
 import type { XyConfig, XyTscConfig } from './XyConfig.ts'
 
 export const packageCompileTypes = async (inConfig: XyConfig = {}): Promise<number> => {
-  const pkg = process.env.INIT_CWD
   const config = await loadConfig(inConfig)
 
   return packageCompileTscTypes(undefined, config as XyTscConfig)
