e4319501d5a4f32fea1ce28386ebf898f73f8ac7
Arie Trouw <arie.trouw@xylabs.com>
2025-04-29 16:44:18 -0700

    Deploy
    6.5.0

--- packages/ts-scripts-yarn3/src/actions/deploy-major.ts
+++ packages/ts-scripts-yarn3/src/actions/deploy-major.ts
@@ -1,11 +1,27 @@
-import { runSteps } from '../lib/index.ts'
+import { readFileSync } from 'node:fs'
+
+import type { Workspace } from '../lib/index.ts'
+import { runSteps, yarnWorkspaces } from '../lib/index.ts'
+
+const privatePackageExcludeList = () => {
+  const possibleDeployablePackages: [Workspace, { private?: boolean }][] = yarnWorkspaces().map(workspace => [workspace,
+    JSON.parse(readFileSync(`${workspace.location}/package.json`, { encoding: 'utf8' })) as object])
+
+  const privatePackages = possibleDeployablePackages.filter(([_, pkg]) => pkg.private).map(([workspace]) => workspace)
+  const excludeList = privatePackages.map(workspace => `--exclude ${workspace.name}`)
+  return excludeList
+}
 
 export const deployMajor = () => {
+  const excludeList = privatePackageExcludeList()
+  if (excludeList.length > 0) {
+    console.log('Excluding private packages from deployment:', excludeList)
+  }
   return runSteps('Deploy [Major]', [
     ['yarn', 'workspaces foreach --all version major --deferred'],
     ['yarn', 'xy clean'],
     ['yarn', 'xy build'],
     ['yarn', 'version apply --all'],
-    ['yarn', 'workspaces foreach --all --parallel npm publish'],
+    ['yarn', `workspaces foreach --all ${excludeList.join(' ')} --parallel npm publish`],
   ])
 }
--- packages/ts-scripts-yarn3/src/actions/deploy-next.ts
+++ packages/ts-scripts-yarn3/src/actions/deploy-next.ts
@@ -1,11 +1,27 @@
-import { runSteps } from '../lib/index.ts'
+import { readFileSync } from 'node:fs'
+
+import type { Workspace } from '../lib/index.ts'
+import { runSteps, yarnWorkspaces } from '../lib/index.ts'
+
+const privatePackageExcludeList = () => {
+  const possibleDeployablePackages: [Workspace, { private?: boolean }][] = yarnWorkspaces().map(workspace => [workspace,
+    JSON.parse(readFileSync(`${workspace.location}/package.json`, { encoding: 'utf8' })) as object])
+
+  const privatePackages = possibleDeployablePackages.filter(([_, pkg]) => pkg.private).map(([workspace]) => workspace)
+  const excludeList = privatePackages.map(workspace => `--exclude ${workspace.name}`)
+  return excludeList
+}
 
 export const deployNext = () => {
+  const excludeList = privatePackageExcludeList()
+  if (excludeList.length > 0) {
+    console.log('Excluding private packages from deployment:', excludeList)
+  }
   return runSteps('Deploy [Next]', [
     ['yarn', 'workspaces foreach --all version minor --deferred'],
     ['yarn', 'xy clean'],
     ['yarn', 'xy build'],
     ['yarn', 'version apply --all --prerelease'],
-    ['yarn', 'workspaces foreach --all --parallel npm publish --tag next'],
+    ['yarn', `workspaces foreach --all ${excludeList.join(' ')} --parallel npm publish --tag next`],
   ])
 }
