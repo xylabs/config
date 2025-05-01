5d08fe53ebe1c379739eff3a064321224e9d8096
Arie Trouw <arie.trouw@xylabs.com>
2025-04-30 17:08:09 -0700

    peer deps check updated

--- packages/ts-scripts-yarn3/src/actions/deplint/checkPackage/getUnusedPeerDependencies.ts
+++ packages/ts-scripts-yarn3/src/actions/deplint/checkPackage/getUnusedPeerDependencies.ts
@@ -3,10 +3,14 @@ import chalk from 'chalk'
 import type { Workspace } from '../../../lib/index.ts'
 import type { CheckPackageParams, CheckSourceParams } from './checkPackageTypes.ts'
 
-export function getUnusedPeerDependencies({ name, location }: Workspace, { peerDependencies }: CheckPackageParams, { externalProdImports }: CheckSourceParams) {
+export function getUnusedPeerDependencies(
+  { name, location }: Workspace,
+  { peerDependencies }: CheckPackageParams,
+  { externalProdImports, externalProdTypeImports }: CheckSourceParams,
+) {
   let unusedDependencies = 0
   for (const dep of peerDependencies) {
-    if (!externalProdImports.includes(dep)) {
+    if (!externalProdImports.includes(dep) && !externalProdTypeImports.includes(dep)) {
       unusedDependencies++
       console.log(`[${chalk.blue(name)}] Unused peerDependency in package.json: ${chalk.red(dep)}`)
       console.log(`  ${location}/package.json\n`)
