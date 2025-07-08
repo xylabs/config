003c68c69a60a7684d9d6dc7a95180a29b811dd4
Arie Trouw <arie.trouw@xylabs.com>
2025-07-08 13:19:04 -0700

    Deploy
    6.5.13

--- packages/ts-scripts-yarn3/src/actions/build.ts
+++ packages/ts-scripts-yarn3/src/actions/build.ts
@@ -14,19 +14,19 @@ export const build = async ({
   incremental, jobs, target, verbose, pkg,
 }: BuildParams) => {
   const start = Date.now()
-  const pkgOptions = pkg ? [pkg] : [] // must go first
+  const pkgOptions = (pkg === undefined) ? [] : [pkg] // must go first
   const incrementalOptions = incremental ? ['-i'] : []
   const verboseOptions = verbose ? ['-v'] : []
-  const targetOptions = target ? ['-t', target] : []
-  const jobsOptions = jobs ? ['-j', `${jobs}`] : []
-  if (jobs) {
+  const targetOptions = (target === undefined) ? [] : ['-t', target]
+  const jobsOptions = (jobs === undefined) ? [] : ['-j', `${jobs}`]
+  if (jobs !== undefined) {
     console.log(chalk.blue(`Jobs set to [${jobs}]`))
   }
 
   const result = await runStepsAsync(`Build${incremental ? '-Incremental' : ''} [${pkg ?? 'All'}]`, [
     ['yarn', ['xy', 'compile', ...pkgOptions, ...targetOptions, ...verboseOptions, ...jobsOptions, ...incrementalOptions, '--types', 'tsup']],
     ['yarn', ['xy', 'publint', ...pkgOptions, ...verboseOptions, ...jobsOptions, ...incrementalOptions]],
-    ['yarn', ['xy', 'knip', ...pkgOptions, ...verboseOptions, ...jobsOptions, ...incrementalOptions]],
+    ['yarn', ['xy', 'deplint', ...pkgOptions, ...verboseOptions, ...jobsOptions, ...incrementalOptions]],
     ['yarn', ['xy', 'lint', ...pkgOptions, ...verboseOptions, ...incrementalOptions]],
   ])
   console.log(`${chalk.gray('Built in')} [${chalk.magenta(((Date.now() - start) / 1000).toFixed(2))}] ${chalk.gray('seconds')}`)
