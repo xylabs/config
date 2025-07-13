1a0c0bdca84095a3cb36460d5615320671664b78
Arie Trouw <arie.trouw@xylabs.com>
2025-07-13 13:49:23 -0700

    Deploy
    7.0.0-rc.19

--- packages/ts-scripts-react-yarn3/xy.config.ts
+++ packages/ts-scripts-react-yarn3/xy.config.ts
@@ -9,11 +9,25 @@ const config: XyTsupConfig = {
 }
 
 // custom - for testing only
-/*
-const config: XyTsupConfig = {
+
+/* const config: XyTsupConfig = {
   compile: {
     browser: { src: { entry: ['index.ts', 'bin/xy.ts'] } },
     entryMode: 'custom',
+    node: { src: { entry: ['index.ts', 'bin/xy.ts'] } },
+    neutral: {},
+  },
+  verbose: true,
+} */
+
+/*
+const config: XyTsupConfig = {
+  compile: {
+    entryMode: 'custom',
+    browser: {
+      'src': { entry: ['index.ts', 'bin/xy.ts'] },
+      'src/bin': { entry: ['xy.ts'] },
+    },
     node: { src: { entry: ['index.ts', 'bin/xy.ts'] } },
     neutral: {},
   },
