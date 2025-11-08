1b6436ae319923914123d0a6bb3d66f48c7bcf76
Arie Trouw <arie.trouw@xylabs.com>
2025-11-08 12:09:12 -0800

    Fully fixed react lint config

--- packages/eslint-config-react-flat/src/react/index.ts
+++ packages/eslint-config-react-flat/src/react/index.ts
@@ -1,21 +1,36 @@
-import eslintReactPlugin from '@eslint-react/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import type { Linter } from 'eslint'
+import reactDomPlugin from 'eslint-plugin-react-dom'
+import reactHooksExtraPlugin from 'eslint-plugin-react-hooks-extra'
+import reactNamingConventionPlugin from 'eslint-plugin-react-naming-convention'
 import eslintReactRefreshPlugin from 'eslint-plugin-react-refresh'
+import reactWebApiPlugin from 'eslint-plugin-react-web-api'
+import reactPlugin from 'eslint-plugin-react-x'
 import globals from 'globals'
 
 export const reactConfig = {
   files: ['**/*.{ts,tsx}'],
-  ...eslintReactPlugin.configs.recommended,
+  ...reactDomPlugin.configs.recommended,
   plugins: {
-    'react': eslintReactPlugin,
+    'react-x': reactPlugin,
+    'react-dom': reactDomPlugin,
+    'react-web-api': reactWebApiPlugin,
     'react-refresh': eslintReactRefreshPlugin,
+    'react-hooks-extra': reactHooksExtraPlugin,
+    'react-naming-convention': reactNamingConventionPlugin,
   },
-  settings: { react: { version: 'detect' } },
+  // settings: { react: { version: 'detect' } },
   languageOptions: {
     parser: tsParser,
     parserOptions: { ecmaFeatures: { jsx: true } },
     globals: { ...globals.browser },
   },
-  rules: { 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }] },
+  rules: {
+    ...reactDomPlugin.configs.recommended.rules,
+    ...reactPlugin.configs.recommended.rules,
+    ...reactWebApiPlugin.configs.recommended.rules,
+    ...reactHooksExtraPlugin.configs.recommended.rules,
+    ...reactNamingConventionPlugin.configs.recommended.rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
 } as unknown as Linter.Config
